d03583d22bcde4319e53b1cf97dbce1af0587eaa
Benjamin Krug <benjaminkrug@yahoo.com>
2014-09-21 16:07:28 -0400

    Part of the work to create new pages

--- Sheepshead/Controllers/GameController.cs
+++ Sheepshead/Controllers/GameController.cs
@@ -12,7 +12,28 @@ namespace Sheepshead.Controllers
         public ActionResult Index()
         {
             var repository = new GameRepository(GameDictionary.Instance.Dictionary);
-            return View(repository.GetOpenGames());
+            return View(repository.GetOpenGames().ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View(String.Empty);
+        }
+
+        [HttpPost]
+        public ActionResult Create(string name)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var newGame = repository.CreateGame(name);
+            var rnd = new Random();
+            var playerQueueRank = rnd.Next(5);
+            for (var i = 0; i < 5; ++i)
+                if (i == playerQueueRank)
+                    newGame.AddPlayer(new HumanPlayer(new User()));
+                else
+                    newGame.AddPlayer(new NewbiePlayer());
+            Session["gameId"] = newGame.Id;
+            return RedirectToAction("Deal");
         }
     }
 }
