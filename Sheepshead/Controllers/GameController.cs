d0acbcaa1f80ab211f282859efba78532387c8a1
Benjamin Krug <benjaminkrug@yahoo.com>
2014-09-28 18:45:18 -0400

    Added Controller methods for Game Play

--- Sheepshead/Controllers/GameController.cs
+++ Sheepshead/Controllers/GameController.cs
@@ -15,6 +15,13 @@ namespace Sheepshead.Controllers
             return View();
         }
 
+        public class GameModel
+        {
+            public string Name { get; set; }
+            public int NewbiewCount { get; set; }
+            public int BasicCount { get; set; }
+        }
+
         public ActionResult Create()
         {
             return View(new GameModel());
@@ -34,14 +41,110 @@ namespace Sheepshead.Controllers
             Session["gameId"] = newGame.Id;
             newGame.RearrangePlayers();
             var deck = new Deck(newGame);
-            return RedirectToAction("Pick");
+            return RedirectToAction("Pick", new { id = newGame.Id });
         }
 
-        public class GameModel
+        public ActionResult Pick(int id)
         {
-            public string Name { get; set; }
-            public int NewbiewCount { get; set; }
-            public int BasicCount { get; set; }
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var game = repository.GetById(id);
+            var deck = new Deck(game);
+            var picker = game.PlayNonHumans(deck);
+            if (picker == null)
+                return View(deck);
+            else
+            {
+                ProcessPick(deck, picker as ComputerPlayer);
+                return RedirectToAction("ReportPick", new { id = game.Id });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Pick(int id, bool willPick, int[] droppedCardsIndex)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var game = repository.GetById(id);
+            var deck = game.Decks.Last();
+            IPlayer picker;
+            if (willPick)
+            {
+                picker = game.Players.First(p => p is HumanPlayer);
+                var droppedCards = droppedCardsIndex.Select(i => picker.Cards[i]).ToList();
+                ProcessPick(deck, picker, droppedCards);
+            }
+            else
+            {
+                picker = game.PlayNonHumans(game.Decks.Last());
+                if (picker == null)
+                    throw new ApplicationException("No one picked");
+                ProcessPick(deck, picker as ComputerPlayer);
+            }
+            return RedirectToAction("ReportPick", new { id = game.Id } );
+        }
+
+        private IHand ProcessPick(IDeck deck, ComputerPlayer picker)
+        {
+            var droppedCards = picker.DropCardsForPick(deck.Hand, picker);
+            return ProcessPick(deck, picker, droppedCards);
+        }
+
+        private IHand ProcessPick(IDeck deck, IPlayer picker, List<ICard> droppedCards)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            return new Hand(deck, picker, droppedCards);
+        }
+
+        public ActionResult ReportPick(int id)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var game = repository.GetById(id);
+            var hand = game.Decks.Last().Hand;
+            return View(hand);
+        }
+
+        public ActionResult PlayTrick(int id)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var game = repository.GetById(id);
+            var hand = game.Decks.Last().Hand;
+            if (hand.IsComplete())
+                throw new ApplicationException("Hand is already complete.");
+            ITrick trick = hand.Tricks.Last();
+            if (trick.IsComplete())
+                trick = new Trick(hand);
+            game.PlayNonHumans(trick);
+            return View(trick);
+        }
+
+        [HttpPost]
+        public ActionResult PlayTrick(int id, int indexOfCard)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var game = repository.GetById(id);
+            var hand = game.Decks.Last().Hand;
+            ITrick trick = hand.Tricks.Last();
+            var player = game.Players.First(p => p is HumanPlayer);
+            var card = player.Cards[indexOfCard];
+            trick.Add(player, card);
+            game.PlayNonHumans(trick);
+            return RedirectToAction("ReportTrick", new { id = id });
+        }
+
+        public ActionResult ReportTrick(int id)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var game = repository.GetById(id);
+            var hand = game.Decks.Last().Hand;
+            ITrick trick = hand.Tricks.Last();
+            return View(trick);
+        }
+
+        public ActionResult ReportHand(int id)
+        {
+            var repository = new GameRepository(GameDictionary.Instance.Dictionary);
+            var game = repository.GetById(id);
+            var hand = game.Decks.Last().Hand;
+            return View(hand);
         }
     }
 }
