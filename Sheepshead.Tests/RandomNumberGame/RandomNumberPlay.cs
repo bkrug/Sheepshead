456755c3425f4910f6346b0edfd976fa1026dd41
Benjamin Krug <benjaminkrug@yahoo.com>
2015-07-05 14:05:55 -0400

    Unsuccessful experiment with moving odds algorithm

---
+++ Sheepshead.Tests/RandomNumberGame/RandomNumberPlay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Sheepshead.Tests.RandomNumberGame
+{
+    [TestClass]
+    public class RandomNumberPlay
+    {
+        [TestMethod]
+        public void PlayRandomNumbers()
+        {
+            var rnd = new Random();
+            var game = new Game(rnd, new EasyToMakePositive(rnd));
+            var playObj = new GamePlayer(game);
+            playObj.Play();
+        }
+    }
+}
