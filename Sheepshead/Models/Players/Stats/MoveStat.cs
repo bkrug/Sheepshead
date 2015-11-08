991a884401f0d71a99cdab1e1b7bb806bb81422b
Benjamin Krug <benjaminkrug@yahoo.com>
2015-11-07 22:44:19 -0500

    Move some variables from class level to passed between methods
    This should protect us from potential situations where two threads may
    be using the same repository instance.

--- Sheepshead/Models/Players/Stats/MoveStat.cs
+++ Sheepshead/Models/Players/Stats/MoveStat.cs
@@ -35,4 +35,18 @@ namespace Sheepshead.Models.Players.Stats
         public int UnknownStrongerCards;
         public int HeldStrongerCards;   //That have not been played
     }
+
+    public class MoveStatConsts
+    {
+        public static Dictionary<string, RangeDetail> MaxRanges = new Dictionary<string, RangeDetail>()
+        {
+            //If OpponentPercentDone were represented as a fraction it would either have a denominator of 3 or 4.  Therefore the precision should have a denominator of 12.
+            { "OpponentPercentDone", new RangeDetail() { Min = 0, Max = 100, ValidValues = new List<int>() { 0, 25, 33, 50, 67, 75, 100 }, Precision = (int)Math.Round(100.0 / 12.0) } },
+            //Giving CardPoints a precision of 1 seems wasteful, but to avoid that I would need to establish some sort of key-value system for points.
+            //Such a decision would hide the fact that 4 points and 10 points are very different numbers.
+            { "CardPoints", new RangeDetail() { Min = -11, Max = 11, ValidValues = new List<int>() { -11, -10, -4, -3, -2, 0, 2, 3, 4, 10, 11 }, Precision = 1 } },
+            { "UnknownStrongerCards", new RangeDetail() { Min = 0, Max = 19 } },
+            { "HeldStrongerCards", new RangeDetail() { Min = 0, Max = 5 } }
+        };
+    }
 }
--- Sheepshead/Models/Players/Stats/ResultPredictor.cs
+++ Sheepshead/Models/Players/Stats/ResultPredictor.cs
@@ -9,10 +9,6 @@ namespace Sheepshead.Models.Players.Stats
     {
         protected readonly IStatRepository<K, S> Repository;
         protected Dictionary<string, RangeDetail> MaxRanges;
-        protected List<List<double>> _nextLayerOfKeys;
-        protected List<double> _steps;
-        protected List<int> _midPoints;
-        protected double _maxRange;
         private const double _tolerance = 0.0001;
         protected int _dimCount;
 
@@ -35,14 +31,15 @@ namespace Sheepshead.Models.Players.Stats
 
         private S GenerateStat(K key)
         {
-            _maxRange = MaxRanges.Max(r => (r.Value.Max - r.Value.Min) / (r.Value.Precision ?? 1));
-            _steps = MaxRanges.Select(r => (r.Value.Max - r.Value.Min) / _maxRange).ToList();
-            _midPoints = GetStartingPoint(key);
-            _nextLayerOfKeys = new List<List<double>>();
             _dimCount = MaxRanges.Count();
+            var maxRange = (double)MaxRanges.Max(r => (r.Value.Max - r.Value.Min) / (r.Value.Precision ?? 1));
+            var steps = MaxRanges.Select(r => (r.Value.Max - r.Value.Min) / maxRange).ToList();
+            var midPoints = GetStartingPoint(key);
+            var nextLayerOfKeys = new List<List<double>>();
             var generatedStat = CreateStat();
-            EditStat(_midPoints.Select(p => (double)p).ToList(), key, ref generatedStat);
-            ExpandStatRange(0, key, ref generatedStat);
+            var midPointsAsDouble = midPoints.Select(p => (double)p).ToList();
+            EditStat(midPointsAsDouble, steps, midPoints, nextLayerOfKeys, key, ref generatedStat);
+            ExpandStatRange(0, maxRange, steps, midPoints, nextLayerOfKeys, key, ref generatedStat);
             return generatedStat;
         }
 
@@ -55,30 +52,30 @@ namespace Sheepshead.Models.Players.Stats
             return startingPoint;
         }
 
-        private void EditStat(List<double> point, K originalKey, ref S generatedStat)
+        private void EditStat(List<double> point, List<double> steps, List<int> midPoints, List<List<double>> nextLayerOfKeys, K originalKey, ref S generatedStat)
         {
             bool skipRecording;
             List<int> recordKey;
-            GetClosestValidKey(point, out skipRecording, out recordKey);
+            GetClosestValidKey(point, steps, midPoints, out skipRecording, out recordKey);
             if (!skipRecording)
             {
                 var newKey = CreateKey(originalKey, recordKey);
                 var recordedStat = Repository.GetRecordedResults(newKey);
                 generatedStat.AddOtherStat(recordedStat);
             }
-            _nextLayerOfKeys.Add(point);
+            nextLayerOfKeys.Add(point);
         }
 
-        private void GetClosestValidKey(List<double> point, out bool skipRecording, out List<int> recordKey)
+        private void GetClosestValidKey(List<double> point, List<double> steps, List<int> midPoints, out bool skipRecording, out List<int> recordKey)
         {
             skipRecording = false;
             recordKey = point.Select(p => (int)Math.Round(p)).ToList();
             for (var i = 0; i < point.Count() && !skipRecording; ++i)
             {
                 var range = MaxRanges.ElementAt(i);
-                var halfOfValidRange = _steps[i] / 2;
+                var halfOfValidRange = steps[i] / 2;
                 //Skip anything that is outside the range of valid values.
-                if (_midPoints[i] - _tolerance > range.Value.Max || _midPoints[i] + _tolerance < range.Value.Min)
+                if (midPoints[i] - _tolerance > range.Value.Max || midPoints[i] + _tolerance < range.Value.Min)
                     skipRecording = true;
                 else if (range.Value.ValidValues != null && range.Value.ValidValues.Any())
                 {
@@ -99,36 +96,36 @@ namespace Sheepshead.Models.Players.Stats
             }
         }
 
-        private void ExpandStatRange(int depth, K originalKey, ref S generatedStat)
+        private void ExpandStatRange(int depth, double maxRange, List<double> steps, List<int> midPoints, List<List<double>> nextLayerOfKeys, K originalKey, ref S generatedStat)
         {
-            if (ReachedMinimumTries(generatedStat) || depth + _tolerance >= _maxRange / 4)
+            if (ReachedMinimumTries(generatedStat) || depth + _tolerance >= maxRange / 4)
                 return;
-            var curLayer = _nextLayerOfKeys;
-            _nextLayerOfKeys = new List<List<double>>();
+            var curLayer = nextLayerOfKeys;
+            nextLayerOfKeys = new List<List<double>>();
             foreach (var point in curLayer)
             {
-                EditChildStats(point, originalKey, ref generatedStat);
+                EditChildStats(point, steps, midPoints, nextLayerOfKeys, originalKey, ref generatedStat);
             }
-            ExpandStatRange(depth + 1, originalKey, ref generatedStat);
+            ExpandStatRange(depth + 1, maxRange, steps, midPoints, nextLayerOfKeys, originalKey, ref generatedStat);
         }
 
         //Expands stat range in n-dimensional space.  If stats could be graphed, they would resemble an n-dimensional diamond or octahedron.
-        private void EditChildStats(List<double> point, K originalKey, ref S generatedStat)
+        private void EditChildStats(List<double> point, List<double> steps, List<int> midPoints, List<List<double>> nextLayerOfKeys, K originalKey, ref S generatedStat)
         {
             var d = 0;
-            while (d == 0 || d < _dimCount && point[d] == _midPoints[d]) 
+            while (d == 0 || d < _dimCount && point[d] == midPoints[d])
             {
-                if (point[d] - _tolerance <= _midPoints[d])
+                if (point[d] - _tolerance <= midPoints[d])
                 {
                     var newPoint = new List<double>(point);
-                    newPoint[d] = point[d] - _steps[d];
-                    EditStat(newPoint, originalKey, ref generatedStat);
+                    newPoint[d] = point[d] - steps[d];
+                    EditStat(newPoint, steps, midPoints, nextLayerOfKeys, originalKey, ref generatedStat);
                 }
-                if (point[d] + _tolerance >= _midPoints[d])
+                if (point[d] + _tolerance >= midPoints[d])
                 {
                     var newPoint = new List<double>(point);
-                    newPoint[d] = point[d] + _steps[d];
-                    EditStat(newPoint, originalKey, ref generatedStat);
+                    newPoint[d] = point[d] + steps[d];
+                    EditStat(newPoint, steps, midPoints, nextLayerOfKeys, originalKey, ref generatedStat);
                 }
                 ++d;
             }
