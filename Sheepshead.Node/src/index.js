7a6930c6da4f68f68cfc62d9e65c99965b885f19
bkrug <benjaminkrug@yahoo.com>
2017-06-04 00:07:42 -0400

    Added React-Router

--- Sheepshead.Node/src/index.js
+++ Sheepshead.Node/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GameSetup from './GameSetup';
+import Mouse from './Mouse'
 import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 ReactDOM.render(
-    <GameSetup />,
+    <Router>
+        <div>
+            <Route exact={true} path="/" component={GameSetup} />
+            <Route path="/Mouse" component={Mouse} />
+        </div>
+    </Router>,
     document.getElementById('root')
 );
