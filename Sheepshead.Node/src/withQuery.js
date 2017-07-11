9782ad5e4bd14e4d6fdd0be180bcd1eebd85a7a0
Benjamin Krug <benjaminkrug@yahoo.com>
2017-07-10 21:43:35 -0400

    Create withQuery() to handle query parameters

---
+++ Sheepshead.Node/src/withQuery.js
@@ -0,0 +1,17 @@
+﻿function getParamAccordingToType(key, queryParams) {
+    var paramValue = queryParams[key];
+    if (Array.isArray(paramValue))
+        return paramValue.map(v => encodeURIComponent(key) + "=" + encodeURIComponent(v)).join('&');
+    else if (typeof paramValue == "object")
+        return encodeURIComponent(key) + "=" + encodeURIComponent(JSON.stringify(paramValue));
+    else
+        return encodeURIComponent(key) + "=" + encodeURIComponent(paramValue);
+}
+
+//Takes a url string and an object and appends to object to the url as query parameters.
+export function withQuery(urlString, queryParams) {
+    var pairs = Object.keys(queryParams)
+          .map(key => getParamAccordingToType(key, queryParams));
+    var firstSeperator = urlString.indexOf('?') >= 0 ? '&' : '?';
+    return urlString + firstSeperator + pairs.join('&');
+};
---
+++ Sheepshead.Node/src/withQuery.test.js
@@ -0,0 +1,46 @@
+﻿import { withQuery } from './withQuery';
+
+test('withQuery - url has string parameters', () => {
+    var expectedUrl = "http://mywebapi.com/home/get?param1=cat&param2=doggy";
+    var actualUrl = withQuery("http://mywebapi.com/home/get", {
+        param1: 'cat',
+        param2: 'doggy'
+    }).toString();
+
+    expect(actualUrl).toEqual(expectedUrl);
+});
+
+test('withQuery - url has string and number parameters', () => {
+    var expectedUrl = "http://mywebapi.com/home/get?param1=cat&smallNo=4782.15&param2=doggy";
+    var actualUrl = withQuery("http://mywebapi.com/home/get", {
+        param1: 'cat',
+        smallNo: 4782.15,
+        param2: 'doggy'
+    }).toString();
+
+    expect(actualUrl).toEqual(expectedUrl);
+});
+
+test('withQuery - url has array', () => {
+    var expectedUrl = "https://catindiapers.com/home/get?requestVal=14&requestVal=-47&requestVal=2.71";
+    var actualUrl = withQuery("https://catindiapers.com/home/get", {
+        requestVal: [14, -47, 2.71]
+    }).toString();
+
+    expect(actualUrl).toEqual(expectedUrl);
+});
+
+test('withQuery - url has an object', () => {
+    var someObj = {
+        something: 12,
+        otherthing: 'qp'
+    };
+    var serialized = encodeURIComponent(JSON.stringify(someObj));
+    var expectedUrl = "https://school.edu/away/put?param3=abcde&param4=" + serialized;
+    var actualUrl = withQuery("https://school.edu/away/put", {
+        param3: 'abcde',
+        param4: someObj
+    }).toString();
+
+    expect(actualUrl).toEqual(expectedUrl);
+});
