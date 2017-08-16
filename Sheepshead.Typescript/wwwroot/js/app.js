c4aa527689fa2ac0ac7bd2f020ec3245c413f16e
Benjamin Krug <benjaminkrug@yahoo.com>
2017-08-16 17:23:48 -0400

    Use typescript in an application.

---
+++ Sheepshead.Typescript/wwwroot/js/app.js
@@ -0,0 +1,9 @@
+//Person is now string
+function Welcome(person) {
+    return "Hello, " + person;
+}
+function ClickMeButton() {
+    var user = "Ben Krug";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
+//# sourceMappingURL=app.js.map
---
+++ Sheepshead.Typescript/wwwroot/js/app.ts
@@ -0,0 +1,9 @@
+﻿//Person is now string
+function Welcome(person: string) {
+    return "Hello, " + person;
+}
+
+function ClickMeButton() {
+    var user = "Ben Krug";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
