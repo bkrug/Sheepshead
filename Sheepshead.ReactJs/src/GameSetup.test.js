4d9a45ee9222b71fbb5e64a65f0c53d0e885261d
Benjamin Krug <benjaminkrug@yahoo.com>
2017-08-17 15:45:34 -0400

    Tests run (but fail) in new project.

---
+++ Sheepshead.ReactJs/src/GameSetup.test.js
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import { shallow } from 'enzyme';
+import GameSetup from '../ClientApp/components/GameSetup.tsx';
+
+test('GameSetup initialy counts 0 players.', () => {
+    const gameSetup = shallow(
+        <GameSetup />
+    );
+
+    expect(gameSetup.find('span.totalPlayers').text()).toEqual("0");
+    expect(gameSetup.find('input.remaining').prop('value')).toEqual(5);
+});
+
+test('GameSetup counts 3 players when we only select 3 humans.', () => {
+    const gameSetup = shallow(
+        <GameSetup />
+    );
+
+    gameSetup.find('PlayerCountRadio[name="humans"]').simulate('change', 'humans', 3);
+    gameSetup.update();
+    expect(gameSetup.find('span.totalPlayers').text()).toEqual("3");
+    expect(gameSetup.find('input.remaining').prop('value')).toEqual(2);
+});
+
+test('GameSetup counts 4 players when we click 3 newbie and 1 learning.', () => {
+    const gameSetup = shallow(
+        <GameSetup />
+    );
+
+    gameSetup.find('PlayerCountRadio[name="newbie"]').simulate('change', 'newbie', 3);
+    gameSetup.find('PlayerCountRadio[name="learning"]').simulate('change', 'learning', 1);
+    gameSetup.update();
+    expect(gameSetup.find('span.totalPlayers').text()).toEqual("4");
+    expect(gameSetup.find('input.remaining').prop('value')).toEqual(1);
+});
+
+test('GameSetup counts 5 players when we click 3 newbie and 1 learning and then change to 4 newbie.', () => {
+    const gameSetup = shallow(
+        <GameSetup />
+    );
+
+    gameSetup.find('PlayerCountRadio[name="newbie"]').simulate('change', 'newbie', 3);
+    gameSetup.find('PlayerCountRadio[name="learning"]').simulate('change', 'learning', 1);
+    gameSetup.find('PlayerCountRadio[name="newbie"]').simulate('change', 'newbie', 4);
+    gameSetup.update();
+    expect(gameSetup.find('span.totalPlayers').text()).toEqual("5");
+    expect(gameSetup.find('input.remaining').prop('value')).toEqual(0);
+});
---
+++ Sheepshead.ReactJs/src/PlayerCountRadio.test.js
@@ -0,0 +1,74 @@
+﻿import React from 'react';
+import { shallow } from 'enzyme';
+import PlayerCountRadio from '../ClientApp/components/PlayerCountRadio.tsx';
+
+test('PlayerCountRadio title is Dwayne', () => {
+    const pcRadio = shallow(
+        <PlayerCountRadio name="fred" title="dwayne"/>
+    );
+
+    expect(pcRadio.find('span.title').text()).toEqual('dwayne');
+});
+
+test('PlayerCountRadio groups have name fred', () => {
+    const pcRadio = shallow(
+        <PlayerCountRadio name="fred" title="dwayne" />
+    );
+
+    pcRadio.find('input[type="radio"]').forEach(function (node) {
+        expect(node.prop('name')).toEqual('fred');
+    });
+});
+
+test('PlayerCountRadio should have new value of 2', () => {
+    const pcRadio = shallow(
+        <PlayerCountRadio name="sam" title="bill" />
+    );
+
+    pcRadio.find('input[type="radio"]').at(2).simulate('click');
+    expect(pcRadio.update().find('.value').props().value).toEqual(2);
+});
+
+test('PlayerCountRadio click event should trigger a change event', () => {
+    var actGroupName, actPlayerCount;
+    var changeFunction = function (groupName, playerCount) { actGroupName = groupName; actPlayerCount = playerCount; };
+    const pcRadio = shallow(
+        <PlayerCountRadio name="dwight" title="dwayne" onChange={changeFunction}/>
+    );
+
+    pcRadio.find('input[type="radio"]').at(3).simulate('click');
+    expect(actGroupName).toEqual("dwight");
+    expect(actPlayerCount).toEqual(3);
+});
+
+test('PlayerCountRadio remaining property of 3 should cause 2 buttons to be disabled.', () => {
+    //The maximum number of players is 5 and 5 - 3 = 2
+
+    const pcRadio = shallow(
+        <PlayerCountRadio name="dwight" title="dwayne" remaining="3"/>
+    );
+
+    var radios = pcRadio.find('input[type="radio"]');
+    expect(radios.at(0).prop('disabled')).toEqual(undefined);
+    expect(radios.at(1).prop('disabled')).toEqual(undefined);
+    expect(radios.at(2).prop('disabled')).toEqual(undefined);
+    expect(radios.at(3).prop('disabled')).toEqual(undefined);
+    expect(radios.at(4).prop('disabled')).toEqual('disabled');
+    expect(radios.at(5).prop('disabled')).toEqual('disabled');
+});
+
+test('PlayerCountRadio remaining property of 2 and value property of 2 should cause 1 buttons to be disabled.', () => {
+    //The maximum number of players is 5 and 5 - 2 - 2 = 1
+
+    const pcRadio = shallow(
+        <PlayerCountRadio name="dwight" title="dwayne" value="2" remaining="2" />
+    );
+
+    var radios = pcRadio.find('input[type="radio"]');
+    expect(radios.at(0).prop('disabled')).toEqual(undefined);
+    expect(radios.at(1).prop('disabled')).toEqual(undefined);
+    expect(radios.at(2).prop('disabled')).toEqual(undefined);
+    expect(radios.at(3).prop('disabled')).toEqual(undefined);
+    expect(radios.at(4).prop('disabled')).toEqual(undefined);
+    expect(radios.at(5).prop('disabled')).toEqual('disabled');
+});
