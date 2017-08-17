9372c91518c9a09fa7a0c2b4e51a49ba0b6a0977
Benjamin Krug <benjaminkrug@yahoo.com>
2017-08-17 02:22:54 -0400

    In new project, GameSetup sort-of renders.

---
+++ Sheepshead.ReactJs/ClientApp/components/GameSetup.tsx
@@ -0,0 +1,146 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'whatwg-fetch';
+//import { withQuery } from './withQuery';
+import PlayerCountRadio from './PlayerCountRadio';
+
+export interface GameSetupState {
+    value: number;
+    remaining: number;
+    gameName: string;
+}
+
+export class GameSetup extends React.Component<any, any> {
+    selections: { [index: string]: number };
+    MAX_PLAYERS = 5;
+    HUMANS: "humans";
+    NEWBIE: "newbie";
+    BASIC: "basic";
+    LEARNING: "learning";
+
+    constructor() {
+        super();
+        this.state = { value: 0, remaining: this.MAX_PLAYERS, gameName: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePlayClick = this.handlePlayClick.bind(this);
+        this.selections = { "a": 0 };
+        this.selections[this.HUMANS] = 0;
+        this.selections[this.NEWBIE] = this.selections[this.BASIC] = this.selections[this.LEARNING] = 0;
+        //this.headerTest();
+    }
+
+    //headerTest() {
+    //    fetch(
+    //        withQuery('http://localhost:61904/api/game/RequestPlayerKey', { oldKey: '1ad8bd99-951d-4d93-a502-8eb0e6ee0a31' }),
+    //        {
+    //            method: 'POST',
+    //            headers: {
+    //                'Accept': 'application/json',
+    //                'Content-Type': 'application/json'
+    //            }
+    //        }
+    //    )
+    //    .then(function (response) {
+    //        return response.json();
+    //    }).then(function (json) {
+    //        console.log('parsed json', json);
+    //        localStorage.setItem('player-key', json);
+    //        return json;
+    //    });
+    //}
+
+    //headerTest2() {
+    //    var playerKey = localStorage.getItem('player-key');
+    //    if (!playerKey)
+    //        throw 'player key was not recorded';
+    //    console.log('player key ' + playerKey);
+    //    fetch(
+    //        'http://localhost:61904/api/game/ValidatePlayerKey?someNumber=5',
+    //        {
+    //            method: 'POST',
+    //            headers: {
+    //                'Accept': 'application/json',
+    //                'Content-Type': 'application/json',
+    //                'Player-Key': playerKey
+    //            }
+    //        }
+    //    )
+    //    .then(function (response) {
+    //        return response.json();
+    //    }).then(function (json) {
+    //        console.log('parsed json', json);
+    //    });
+    //}
+
+    handleChange(radioGroup: string, radioValue: number) {
+        //this.headerTest2();
+        if (radioValue === 0 || radioValue > 0) {
+            this.selections[radioGroup] = radioValue;
+            var newTotal = this.selections[this.HUMANS] + this.selections[this.NEWBIE] + this.selections[this.BASIC] + this.selections[this.LEARNING];
+            this.setState({ value: newTotal, remaining: this.MAX_PLAYERS - newTotal });
+        }
+    }
+
+    handleNameChange(e: React.FormEvent<HTMLInputElement>) {
+        console.log(e.currentTarget.value);
+        this.setState({ gameName: e.currentTarget.value });
+    }
+
+    playerCountValidityClass() {
+        return (this.state.value === 3 || this.state.value === 5) ? '' : 'invalidcount';
+    }
+
+    handlePlayClick() {
+        var gameStartModel = {
+            GameName: this.state.gameName,
+            HumanCount: this.selections[this.HUMANS],
+            NewbieCount: this.selections[this.NEWBIE],
+            BasicCount: this.selections[this.BASIC],
+            LearningCount: this.selections[this.LEARNING]
+        };
+        fetch(
+            'http://localhost:61904/api/game/create',
+            {
+                method: 'POST',
+                body: JSON.stringify(gameStartModel),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                console.log('parsed json', json)
+                if (json.success === true) { }
+                    //document.location = "/Shuffle";
+                else
+                    throw new Error("Unknown server-side error occurred.");
+            }).catch(function (ex) {
+                console.log('parsing failed', ex)
+            });
+    }
+
+    public render() {
+        return (
+            <div className="gameSetup">
+                <h4>Setup Sheepshead Game</h4>
+                <label>Game Name</label>
+                <input id="name-input" type="text" onChange={ e => this.handleNameChange(e) } value={this.state.gameName} />
+                <PlayerCountRadio name={this.HUMANS} title="Humans" onChange={this.handleChange} remaining={this.state.remaining} />
+                <PlayerCountRadio name={this.NEWBIE} title="A.I. Simple" onChange={this.handleChange} remaining={this.state.remaining} />
+                <PlayerCountRadio name={this.BASIC} title="A.I. Basic" onChange={this.handleChange} remaining={this.state.remaining} />
+                <PlayerCountRadio name={this.LEARNING} title="A.I. Statistic" onChange={this.handleChange} remaining={this.state.remaining} />
+                <div>
+                    <span>Total Players:</span>
+                    <span className={"totalPlayers " + this.playerCountValidityClass()}>{this.state.value}</span>
+                </div>
+                <input type="hidden" className="remaining" value={this.state.remaining} />
+                <input type="button" value="Play" onClick={this.handlePlayClick} />
+            </div>
+        );
+    }
+}
---
+++ Sheepshead.ReactJs/ClientApp/components/PlayerCountRadio.tsx
@@ -0,0 +1,66 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export interface PlayerCountState {
+    title: string;
+    value: number;
+    onChange: any;
+    name: string;
+    remaining: number;
+}
+
+//export default class PlayerCountRadio extends React.Component<RouteComponentProps<{}>, PlayerCountState> {
+export default class PlayerCountRadio extends React.Component<any, any> {
+    constructor(props: PlayerCountState) {
+        super(props);
+        this.state = {
+            value: props.value || 0,
+            title: props.title,
+            onChange: props.onChange,
+            name: props.name,
+            remaining: props.remaining
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(i:number) {
+        this.setState({ value: i });
+        if (this.state.onChange)
+            this.state.onChange(this.state.name, i);
+    }
+
+    getValue() {
+        return this.state.value;
+    }
+
+    renderRadio(i: number) {
+        return (
+            <span>
+                <input
+                    type="radio"
+                    name={this.state.name}
+                    value={i}
+                    onClick={() => this.handleClick(i)}
+                    onChange={() => function () { return; }}
+                    checked={this.state.value === i}
+                    disabled={this.state.remaining + this.state.value < i}
+                />{i}
+            </span>
+        );
+    }
+
+    public render() {
+        return (
+            <div className="playerCountRadio" onChange={this.state.onChange}>
+                <span className="title">{this.state.title}</span>
+                {this.renderRadio(0)}
+                {this.renderRadio(1)}
+                {this.renderRadio(2)}
+                {this.renderRadio(3)}
+                {this.renderRadio(4)}
+                {this.renderRadio(5)}
+                <input type="hidden" className="value" value={this.state.value} />
+            </div>
+        );
+    }
+}
