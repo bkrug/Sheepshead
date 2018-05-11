e20dd647c2119b9a914afd00c38f89142638fe10
Benjamin Krug <benjaminkrug@yahoo.com>
2018-05-10 23:36:52 -0400

    Created a React project

---
+++ Sheepshead.React/ClientApp/components/setup/GameSetup.tsx
@@ -0,0 +1,101 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'whatwg-fetch';
+//import { withQuery } from './withQuery';
+import PlayerCountRadio from './PlayerCountRadio';
+
+export interface GameSetupState {
+    value: number;
+    remaining: number;
+    gameName: string;
+    selections: { [index: string]: number };
+}
+
+export class GameSetup extends React.Component<RouteComponentProps<{}>, GameSetupState> {
+    readonly MAX_PLAYERS = 5;
+    readonly HUMANS = "humans";
+    readonly NEWBIE = "newbie";
+    readonly BASIC = "basic";
+
+    constructor() {
+        super();
+        let selections: { [index: string]: number } = {};
+        selections[this.HUMANS] = selections[this.NEWBIE] = selections[this.BASIC] = 0;
+        this.state = { value: 0, remaining: this.MAX_PLAYERS, gameName: '', selections: selections };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePlayClick = this.handlePlayClick.bind(this);
+    }
+
+    handleChange(radioGroup: string, radioValue: number) {
+        //this.headerTest2();
+        if (radioValue === 0 || radioValue > 0) {
+            this.state.selections[radioGroup] = radioValue;
+            let newTotal = this.state.selections[this.HUMANS]
+                + this.state.selections[this.NEWBIE]
+                + this.state.selections[this.BASIC];
+            this.setState({ value: newTotal, remaining: this.MAX_PLAYERS - newTotal });
+        }
+    }
+
+    handleNameChange(e: React.FormEvent<HTMLInputElement>) {
+        console.log(e.currentTarget.value);
+        this.setState({ gameName: e.currentTarget.value });
+    }
+
+    playerCountValidityClass() {
+        return (this.state.value === 3 || this.state.value === 5) ? '' : 'invalidcount';
+    }
+
+    handlePlayClick() {
+        var gameStartModel = {
+            GameName: this.state.gameName,
+            HumanCount: this.state.selections[this.HUMANS],
+            NewbieCount: this.state.selections[this.NEWBIE],
+            BasicCount: this.state.selections[this.BASIC]
+        };
+        fetch(
+            'http://localhost:61904/api/game/create',
+            {
+                method: 'POST',
+                body: JSON.stringify(gameStartModel),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                console.log('parsed json', json)
+                if (json.success === true) { }
+                //document.location = "/Shuffle";
+                else
+                    throw new Error("Unknown server-side error occurred.");
+            })
+            .catch(function (ex) {
+                console.log('parsing failed', ex)
+            });
+    }
+
+    public render() {
+        return (
+            <div className="gameSetup">
+                <h4>Setup Sheepshead Game</h4>
+                <label>Game Name</label>
+                <input id="name-input" type="text" onChange={e => this.handleNameChange(e)} value={this.state.gameName} />
+                <PlayerCountRadio name={this.HUMANS} title="Humans" onChange={this.handleChange} value={this.state.selections[this.HUMANS]} remaining={this.state.remaining} />
+                <PlayerCountRadio name={this.NEWBIE} title="A.I. Simple" onChange={this.handleChange} value={this.state.selections[this.NEWBIE]} remaining={this.state.remaining} />
+                <PlayerCountRadio name={this.BASIC} title="A.I. Basic" onChange={this.handleChange} value={this.state.selections[this.BASIC]} remaining={this.state.remaining} />
+                <div>
+                    <span>Total Players:</span>
+                    <span className={"totalPlayers " + this.playerCountValidityClass()}>{this.state.value}</span>
+                </div>
+                <input type="hidden" className="remaining" value={this.state.remaining} />
+                <input type="button" value="Play" onClick={this.handlePlayClick} />
+            </div>
+        );
+    }
+}
---
+++ Sheepshead.React/ClientApp/components/setup/PlayerCountRadio.tsx
@@ -0,0 +1,66 @@
+﻿import '../../css/setup.css';
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export interface PlayerCountState {
+    title: string;
+    value: number;
+    onChange: any;
+    name: string;
+}
+
+//export default class PlayerCountRadio extends React.Component<RouteComponentProps<{}>, PlayerCountState> {
+export default class PlayerCountRadio extends React.Component<any, any> {
+    constructor(props: PlayerCountState) {
+        super(props);
+        this.state = {
+            value: props.value || 0,
+            title: props.title,
+            onChange: props.onChange,
+            name: props.name
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(i: number) {
+        if (this.state.onChange)
+            this.state.onChange(this.state.name, i);
+    }
+
+    getValue() {
+        return this.props.value;
+    }
+
+    renderRadio(i: number) {
+        return (
+            <span>
+                <input
+                    type="radio"
+                    name={this.state.name}
+                    value={i}
+                    onClick={() => this.handleClick(i)}
+                    onChange={() => function () { return; }}
+                    checked={this.props.value === i}
+                    disabled={this.props.remaining + this.props.value < i}
+                />{i}
+            </span>
+        );
+    }
+
+    public render() {
+        return (
+            <div className="playerCountRadio" onChange={this.state.onChange}>
+                <div className="title-container"><span className="title">{this.props.title}</span></div>
+                <div className="button-container">
+                    {this.renderRadio(0)}
+                    {this.renderRadio(1)}
+                    {this.renderRadio(2)}
+                    {this.renderRadio(3)}
+                    {this.renderRadio(4)}
+                    {this.renderRadio(5)}
+                </div>
+                <input type="hidden" className="value" value={this.props.value} />
+            </div>
+        );
+    }
+}
