2f45059e3a4c06014c01fb1afa3eca62125aac25
Benjamin Krug <benjaminkrug@yahoo.com>
2018-05-11 22:59:19 -0400

    Delete User and UserRepository

--- Sheepshead.Tests/PlayerTests.cs
+++ Sheepshead.Tests/PlayerTests.cs
@@ -12,16 +12,6 @@ namespace Sheepshead.Tests
     public class PlayerTests
     {
         [TestMethod]
-        public void Player_HasSameNameAsUser()
-        {
-            var expectedName = "NameOfSomeoneImportant";
-            var user = new Mock<IUser>();
-            user.Setup(m => m.Name).Returns(expectedName);
-            var player = new HumanPlayer(user.Object);
-            Assert.AreEqual(expectedName, player.Name, "Name of player object and user object match.");
-        }
-
-        [TestMethod]
         public void NewbiePlayer_GetMove()
         {
             var mainPlayer = new NewbiePlayer();
