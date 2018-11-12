a2b25c282b8e750e1069fed08c28f37696ce35e2
Benjamin Krug <benjaminkrug@yahoo.com>
2018-11-11 20:16:37 -0500

    Wrote some tests for Playing the strongest and weakest cards.

---
+++ Sheepshead.Tests/MidTrickPlayCreatorTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sheepshead.Models;
+using Sheepshead.Models.Players;
+
+namespace Sheepshead.Tests
+{
+    [TestClass]
+    public class MidTrickPlayCreatorTests
+    {
+        [TestMethod]
+        public void MidTrickPlayCreator_PlayWeakestWin()
+        {
+            var pickerMock = new Mock<IPlayer>();
+            var partnerMock = new Mock<IPlayer>();
+            var playerMock = new Mock<IPlayer>();
+            var playerCards = new List<SheepCard>()
+            {
+                SheepCard.KING_HEARTS,
+                SheepCard.JACK_DIAMONDS,
+                SheepCard.N10_SPADES,
+                SheepCard.QUEEN_CLUBS
+            };
+            playerMock.Setup(m => m.Cards).Returns(playerCards);
+            var cardsPlayed = new Dictionary<IPlayer, SheepCard>() {
+                { new Mock<IPlayer>().Object, SheepCard.N7_DIAMONDS },
+                { new Mock<IPlayer>().Object, SheepCard.ACE_SPADES },
+                { new Mock<IPlayer>().Object, SheepCard.ACE_DIAMONDS }
+            };
+            var trickMock = new Mock<ITrick>();
+            trickMock.Setup(m => m.CardsPlayed).Returns(cardsPlayed);
+            trickMock.Setup(m => m.IsLegalAddition(SheepCard.JACK_DIAMONDS, playerMock.Object)).Returns(true);
+            trickMock.Setup(m => m.IsLegalAddition(SheepCard.QUEEN_CLUBS, playerMock.Object)).Returns(true);
+            var playCreator = new MidTrickPlayCreator();
+            var actual = playCreator.PlayWeakestWin(playerMock.Object, trickMock.Object);
+            Assert.AreEqual(SheepCard.JACK_DIAMONDS, actual);
+        }
+
+        [TestMethod]
+        public void MidTrickPlayCreator_PlayStrongestWin()
+        {
+            var pickerMock = new Mock<IPlayer>();
+            var partnerMock = new Mock<IPlayer>();
+            var playerMock = new Mock<IPlayer>();
+            var playerCards = new List<SheepCard>()
+            {
+                SheepCard.KING_HEARTS,
+                SheepCard.JACK_DIAMONDS,
+                SheepCard.N10_SPADES,
+                SheepCard.QUEEN_CLUBS
+            };
+            playerMock.Setup(m => m.Cards).Returns(playerCards);
+            var cardsPlayed = new Dictionary<IPlayer, SheepCard>() {
+                { new Mock<IPlayer>().Object, SheepCard.N7_DIAMONDS },
+                { new Mock<IPlayer>().Object, SheepCard.ACE_SPADES },
+                { new Mock<IPlayer>().Object, SheepCard.ACE_DIAMONDS }
+            };
+            var trickMock = new Mock<ITrick>();
+            trickMock.Setup(m => m.CardsPlayed).Returns(cardsPlayed);
+            trickMock.Setup(m => m.IsLegalAddition(SheepCard.JACK_DIAMONDS, playerMock.Object)).Returns(true);
+            trickMock.Setup(m => m.IsLegalAddition(SheepCard.QUEEN_CLUBS, playerMock.Object)).Returns(true);
+            var playCreator = new MidTrickPlayCreator();
+            var actual = playCreator.PlayStrongestWin(playerMock.Object, trickMock.Object);
+            Assert.AreEqual(SheepCard.QUEEN_CLUBS, actual);
+        }
+    }
+}
