7303ca3e36dc413604c945786db8d3e404d7858d
Benjamin Krug <benjaminkrug@yahoo.com>
2018-12-02 17:39:18 -0500

    Remove duplicate reference in AssemblyInfo

--- Sheepshead/Properties/AssemblyInfo.cs
+++ Sheepshead/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("60caed58-ba59-4eb5-8643-6bd3d932a273")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
